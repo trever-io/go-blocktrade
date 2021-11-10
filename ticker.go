298d9c81a74b3a42a0380f42b5abbfd7ec9e873a
Mathias Kahr <m.kahr@student.tugraz.at>
2021-11-10 08:56:15 +0100

    add ticker get endpoint

--- ticker.go
+++ ticker.go
@@ -1,5 +1,12 @@
 package blocktrade
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+const TICKER_ENDPOINT = "/ticker"
+
 type TickerData struct {
 	AskPrice  string `json:"ask_price"`
 	BidPrice  string `json:"bid_price"`
@@ -12,4 +19,16 @@ type TickerData struct {
 type TickerResponse struct {
 	TradingPairId int64      `json:"trading_pair_id"`
 	Data          TickerData `json:"data"`
+}
+
+func (a *APIClient) GetTicker(tradingPairId int64) (*TickerData, error) {
+	url := fmt.Sprintf("%v/%d", TICKER_ENDPOINT, tradingPairId)
+	b, err := a.requestPublicGET(url)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := new(TickerData)
+	err = json.Unmarshal(b, resp)
+	return resp, err
 }
